227f1e3d08af3b6c2f4233efe7d2217c58700aed
black-coder <black-coder@BLACK-CODER>
2023-03-06 22:06:49 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BlackMultipleDoCoder.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
